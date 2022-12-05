7d96228fe5fcf6569ac4c3959c2aafb05ceb8902
Firstname Lastname <57352940+sherkhantazhibayev@users.noreply.github.com>
2022-12-06 00:24:18 +0600

    Add files via upload

--- untitled/src/main/java/org/example/App.java
+++ untitled/src/main/java/org/example/App.java
@@ -1,13 +1,36 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Person person1 = new Person("test1", 10);
+            Person person2 = new Person("test2", 20);
+            Person person3 = new Person("test3", 30);
+            Person person4 = new Person("test4", 40);
+
+            session.save(person1);
+            session.save(person2);
+            session.save(person3);
+            session.save(person4);
+
+            session.getTransaction().commit();
+
+        } finally {
+            sessionFactory.close();
+        }
+
+
     }
 }
