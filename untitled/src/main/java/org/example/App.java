942cabff02c5c72d49231f1ec0c2020f6bf347e4
Sherkhan <tazhibayevsherkhan7@gmail.com>
2022-12-08 23:53:47 +0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	untitled/src/main/java/org/example/App.java

--- untitled/src/main/java/org/example/App.java
+++ untitled/src/main/java/org/example/App.java
@@ -19,6 +19,14 @@ public class App {
             Person person2 = new Person("test2", 20);
             Person person3 = new Person("test3", 30);
             Person person4 = new Person("test4", 40);
+            Person person5 = new Person("test5", 10);
+            Person person6 = new Person("test6", 20);
+            Person person7 = new Person("test7", 30);
+            Person person8 = new Person("test8", 40);
+            Person person9 = new Person("test9", 10);
+            Person person10 = new Person("test10", 20);
+            Person person11 = new Person("test11", 30);
+            Person person12 = new Person("test12", 40);
 
             session.save(person1);
             session.save(person2);
